b4ccc685bcfbe044eb5799f50e64d7fd9776f3fa
Trevor Keith <tk@tkeith.com>
2025-04-11 12:36:36 -0400

    update default app title/description

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,9 +23,8 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       ...seo({
-        title:
-          "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+        title: "My App",
+        description: "A full-stack application powered by TanStack and Codapt",
       }),
     ],
     links: [
