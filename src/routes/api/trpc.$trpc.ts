b665670df89cbaefbaa0801d433984c22b9110bd
Trevor Keith <tk@tkeith.com>
2025-04-23 12:14:25 -0700

    tRPC OPTIONS handler & allow CORS

--- src/routes/api/trpc.$trpc.ts
+++ src/routes/api/trpc.$trpc.ts
@@ -3,41 +3,37 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
+const setCorsHeaders = (res: Response) => {
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Allow-Headers", "*");
+};
+
+const handler = async ({ request }: { request: Request }) => {
+  const response = await fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req: request,
+    router: appRouter,
+    createContext: () => createTRPCContext({ headers: request.headers }),
+    onError: ({ path, error }) => {
+      console.error(
+        `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+      );
+    },
+  });
+
+  setCorsHeaders(response);
+
+  return response;
+};
+
+const optionsHandler = async ({ request }: { request: Request }) => {
+  const response = new Response();
+  setCorsHeaders(response);
+  return response;
+};
+
 export const APIRoute = createAPIFileRoute("/api/trpc/$trpc")({
-  GET: async ({ request }) => {
-    const response = await fetchRequestHandler({
-      endpoint: "/api/trpc",
-      req: request,
-      router: appRouter,
-      createContext: () => createTRPCContext({ headers: request.headers }),
-      onError:
-        process.env.NODE_ENV === "development"
-          ? ({ path, error }) => {
-              console.error(
-                `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-              );
-            }
-          : undefined,
-    });
-
-    return response;
-  },
-  POST: async ({ request }) => {
-    const response = await fetchRequestHandler({
-      endpoint: "/api/trpc",
-      req: request,
-      router: appRouter,
-      createContext: () => createTRPCContext({ headers: request.headers }),
-      onError:
-        process.env.NODE_ENV === "development"
-          ? ({ path, error }) => {
-              console.error(
-                `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-              );
-            }
-          : undefined,
-    });
-
-    return response;
-  },
+  GET: handler,
+  POST: handler,
+  OPTIONS: optionsHandler,
 });
